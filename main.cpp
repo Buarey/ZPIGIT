2a57fe4820fc676a36ef326e5868b61e996bbbfd
Buarey <render@borg-tech.pl>
2020-03-14 18:12:20 +0100

    Add main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    cout << "Hello, World!" << endl
+         << "Drugi commit" << endl;
     return 0;
 }
